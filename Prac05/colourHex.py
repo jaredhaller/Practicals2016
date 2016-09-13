540dec3eb9dbfecd1151b038e3934bfd15beeea8
Jared Haller <jared19961@hotmail.com>
2016-09-13 17:41:23 +1000

    Finished colourHex.py

---
+++ Prac05/colourHex.py
@@ -0,0 +1,16 @@
+colours_id = {"alice blue": "#f0f8ff", "blanched almond": "#ffebcd", "black": "#00000", "blue": "#0000ff",
+              "blue violet": "#8a2be2", "brown": "#a52a2a","cadet blue": "#5f9ea0", "chocolate": "#d2691e",
+              "cornflower blue": "#6495ed", "dark green": "#006400", }
+
+# print(colours_id)
+for key, value in colours_id.items():
+    print("{:<17} is {}".format(key.title(), value))
+
+colour = input("Enter colour name: ").lower()
+while colour != "":
+    if colour in colours_id:
+        print(colour.title(), "is", colours_id[colour])
+    else:
+        print("Invalid colour name")
+    colour = input("Enter colour name: ").lower()
+;

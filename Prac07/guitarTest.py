d1ce40cef182a42b6c6476df8ea3b123c847a347
Jared Haller <jared19961@hotmail.com>
2016-10-18 20:09:42 +1000

    Add necessary features to guitar.py and guitarTest.py to finish program

--- Prac07/guitarTest.py
+++ Prac07/guitarTest.py
@@ -1,3 +1,37 @@
 """
 Do from scratch exercise
 """
+
+from Prac07.guitar import Guitar
+
+
+def main():
+
+    guitar_list = []
+
+    while True:
+        name = input("Name: ")
+
+        if not name:
+            break
+
+        year = int(input("Year: "))
+        price = float(input("Price: "))
+        guitar = Guitar(name, year, price)
+
+        guitar_list.append(guitar)
+        print(guitar)
+
+    counter = 1
+
+    for guitar in guitar_list:
+        if guitar.is_vintage():
+            print("Guitar {}: {:>20} ({}), worth ${:10,.2f} (vintage)".format(counter, guitar.name, guitar.year,
+                                                                              guitar.cost))
+        else:
+            print("Guitar {}: {:>20} ({}), worth ${:10,.2f}".format(counter, guitar.name, guitar.year, guitar.cost))
+
+        counter += 1
+
+
+main()

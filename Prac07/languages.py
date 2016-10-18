815537069dd41d737ed44d769f2612e6f18acd5c
Jared Haller <jared19961@hotmail.com>
2016-10-18 16:16:33 +1000

    Add necessary features to languages.py and programminglanguage.py finishing program

--- Prac07/languages.py
+++ Prac07/languages.py
@@ -1,3 +1,26 @@
 """
-Intermediate Exerice
+Intermediate Exercise
 """
+
+from Prac07.programminglanguage import ProgrammingLanguage
+
+
+def main():
+    # ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    # python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    # vb = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    #
+    # print(ruby)
+    # print(python)
+    # print(vb)
+
+    languages = [ProgrammingLanguage("Ruby", "Dynamic", True, 1995),
+                 ProgrammingLanguage("Python", "Dynamic", True, 1991),
+                 ProgrammingLanguage("Visual Basic", "Static", False, 1991)]
+
+    print("The dynamically typed languages are:")
+
+    for language in [language for language in languages if language.is_dynamic()]:
+        print(language)
+
+main()
--- Prac07/programminglanguage.py
+++ Prac07/programminglanguage.py
@@ -1,3 +1,20 @@
 """
-Intermediate Exerice
+Intermediate Exercise
 """
+
+
+class ProgrammingLanguage:
+
+    def __init__(self, language, typing, reflection, year):
+        self.language = language
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        return True if self.typing is 'Dynamic' else False
+
+    def __str__(self):
+        # return "{}, {} Typing, Reflection={}, First appear in {}"\
+        #     .format(self.language, self.typing, self.is_dynamic(), self.year)
+        return "{}".format(self.language)

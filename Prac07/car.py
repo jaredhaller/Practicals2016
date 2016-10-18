a7c224e41cd04788f4156689acea569301401440
Jared Haller <jared19961@hotmail.com>
2016-10-18 15:33:34 +1000

    Add features to car.py and usedCar.py finishing

--- Prac07/car.py
+++ Prac07/car.py
@@ -5,11 +5,12 @@ Car class example
 
 
 class Car:
-    def __init__(self, fuel=0):
+    def __init__(self, name, fuel=0):
         """ initialise a Car instance
         fuel: float, one unit of fuel drives one kilometre """
         self.fuel = fuel
         self.odometer = 0
+        self.name = name
 
     def add_fuel(self, amount):
         """ add amount to the car's fuel"""
@@ -26,3 +27,6 @@ class Car:
             self.fuel -= distance
         self.odometer += distance
         return distance
+
+    def __str__(self):
+        return "{}, fuel={}, odometer={}".format(self.name, self.fuel, self.odometer)
--- Prac07/usedCar.py
+++ Prac07/usedCar.py
@@ -7,13 +7,20 @@ from Prac07.car import Car
 
 
 def main():
-    bus = Car(180)
+    bus = Car('Bus', 180)
     bus.drive(30)
     print("fuel =", bus.fuel)
     print("odo =", bus.odometer)
     print(bus)
 
-    print("Car {}, {}".format(bus.fuel, bus.odometer))
-    print("Car {self.fuel}, {self.odometer}".format(self=bus))
+    limo = Car('Limo', 100)
+    limo.fuel += 20
+    limo.drive(115)
+    print("fuel =", limo.fuel)
+    print("odo =", limo.odometer)
+    print(limo)
+
+    # print("Car {}, {}".format(bus.fuel, bus.odometer))
+    # print("Car {self.fuel}, {self.odometer}".format(self=bus))
 
 main()
